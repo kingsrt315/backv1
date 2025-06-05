6954f1004d19eba0950f73907430694fab58644c
GhaithQ1 <ghaithdrh@gmail.com>
2025-06-05 18:59:15 +0300

    .

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(express.json({
 }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://front-v1-orcin.vercel.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
